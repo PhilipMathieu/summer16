010ee97bc1872d04e2c72d921b6756db651ced62
PhilipMathieu <philip_mathieu@brown.edu>
2016-06-09 16:28:56 -0400

    Upgraded conversion script to run on directories

--- useful_code/miriad2fits.py
+++ useful_code/miriad2fits.py
@@ -1,18 +1,27 @@
 #! /usr/bin/env python
 
 import argparse
+import os
 from uvdata.uv import UVData
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mir', help='Path of input Miriad file')
-parser.add_argument('--fits', help='Path of output UVFits file',default='none')
+parser.add_argument('-d',action='store_true')
+parser.add_argument('mir', help='Path of input Miriad file')
+parser.add_argument('-f','--fits', help='Path of output UVFits file')
 args = parser.parse_args()
-infile = args.mir
-if args.fits == 'none':
-    outfile = args.mir+'.fits'
+print args
+if args.d:
+    mdir = args.mir
+    files = os.listdir(mdir)
 else:
-    outfile = args.fits
-this_uv = UVData()
-this_uv.read_miriad(infile)
-this_uv.write_uvfits(outfile,True,True)
-del(this_uv)
+    files = [args.mir]
+print files
+for infile in files:
+    if not(args.fits == None) and not args.d:
+        outfile = args.fits
+    else:
+        outfile = infile+'.fits'
+    this_uv = UVData()
+    this_uv.read_miriad(infile)
+    this_uv.write_uvfits(outfile,True,True)
+    del(this_uv)
