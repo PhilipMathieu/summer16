4842cb961fb6a7a833da3d854d67fed12342fb4b
PhilipMathieu <philip_mathieu@brown.edu>
2016-06-17 12:08:24 -0400

    Various tweaks to conversion script including a section for checking/correcting UVProperty's

--- useful_code/miriad2fits.py
+++ useful_code/miriad2fits.py
@@ -16,13 +16,24 @@ if args.d:
         filename = mdir+filename
 else:
     files = [args.mir]
-print files
+    #Check if trailing slash from autocomplete
+    if files[0][-1:] == '/':
+        print 'WARNING: Trailing slash, but not in directory mode.  Ignoring...'
+        files[0] = files[0][:-1]
 for infile in files:
     if not(args.fits == None) and not args.d:
         outfile = args.fits
     else:
-        outfile = infile+'.fits'
+        outfile = infile+'.uvfits'
     this_uv = UVData()
     this_uv.read_miriad(infile)
+    #This is hacky rn but could be reworked into a more comprehensive system for
+    #fixing expected problems with the input file, possibly via an input file or 
+    #something that allows you to manually correct UVProperty values and such
+    if this_uv.telescope_name == 'PAPER':
+        if this_uv.longitude < 0:
+            this_uv.telescope_name='PAPER_GB'
+        else:
+            this_uv.telescope_name='PAPER_SA'
     this_uv.write_uvfits(outfile,True,True)
     del(this_uv)
