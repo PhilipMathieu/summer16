192329be8c5996f8b328917e98c85de7aee68162
Philip Mathieu <philip_mathieu@brown.edu>
2016-08-04 15:33:27 -0400

    Partially fixed virtualenv, added installation and requirements info

---
+++ hera-noise/install_requirements.sh
@@ -0,0 +1,5 @@
+# this script installs the required packages in an order that makes pip happy
+pip install numpy pyephem pyfits
+pip install astropy
+pip install aipy
+# pyuvdata must be installed manually
---
+++ hera-noise/test3/lib/python2.7/copy_reg.py
@@ -0,0 +1,1 @@
+/gpfs/runtime/opt/python/2.7.3/lib/python2.7/copy_reg.py
